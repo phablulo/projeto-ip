273fc1bf0b5fb0c7ee43eaaf9a1ab664d6e1f3ba
Robson Cicero da Silva <rcs9@WINDOWS.CIN.UFPE.BR>
2017-11-14 16:05:58 -0300

    Pequenas mudanças nos atributos de Pessoa e adicionados os diretorios dos packages/camadas do programa.

--- Pessoa.java
+++ Pessoa.java
@@ -1,9 +1,11 @@
 abstract class Pessoa {
 	private String nome;
 	private String cpf;
-	private String idade;
-
+	private String datanasc;
+	private String endereco;
+	
+	
 	public Livro procura(String nome) {
-		return new Livro("1A", "Nome temporário", new String[]{"Phablulo"}, 42, 42.0);
+		return new Livro("1A", "Nome temporario", new String[]{"Phablulo"}, 42, 42.0);
 	}
 }
