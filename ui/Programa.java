12987acb5411fe463b5e9da1c14530a6f3b3fd88
Phablulo <phablulo@live.com>
2017-11-20 16:18:20 -0300

    início da classe Programa (fachada)

--- ui/Programa.java
+++ ui/Programa.java
@@ -1,9 +1,15 @@
 package ui;
+import dados.*;
 
 /*Classe para testar o programa e tratar das exceptions*/
 
 public class Programa {
 	public static void main(String[] args) {
-		
+
+	}
+	public static void useLista() {
+		Livro alquimista = new Livro("001", "O Alquimista", new String[]{"Paulo Coelho"}, 93, 25.0);
+		Livro narnia     = new Livro("001", "As Crônicas de Nárnia", new String[]{"C.S. Lewis"}, 800, 80.0);
+
 	}
 }
