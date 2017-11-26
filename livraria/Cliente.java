a6f32be13b839d45ae78e35550c4d8e5bf2af0a4
Phablulo <phablulo@live.com>
2017-11-26 16:49:26 -0300

    correcao na classe de thomas

--- livraria/Cliente.java
+++ livraria/Cliente.java
@@ -1,38 +1,32 @@
+package livraria;
+
 public class Cliente extends Pessoa {
 	String nome;
 	String cpf;
-	String ClienteId;
-	
+	String clienteId;
+
 	/*Adequar os metodos e construtores a classe abstrata usando a keyword super.
-	 * Reforcando, o uso de Ids pode nao ser tao interessante, cpf seria uma alternativa melhor e mais rápida para implementar.*/ 
-	
-	public Cliente(String nome, String cpf, String ClienteId) {
-		this.nome = nome;
-		this.cpf = cpf;
-		this.ClienteId = ClienteId;
+	 * Reforcando, o uso de Ids pode nao ser tao interessante, cpf seria uma alternativa melhor e mais rapida para implementar.*/
+
+	public Cliente(String nome, String cpf) {
+		super (nome, cpf);
 	}
-	//Segundo construtor a ser implementado de acordo com a classe abstrata. Se necessario, observe como ficou a classe Funcionario.
+	// Segundo construtor a ser implementado de acordo com a classe abstrata. Se necessario, observe como ficou a classe Funcionario.
 	public Cliente(String nome, String cpf, String datanasc, String endereco) {
-		
+		super (nome, cpf, datanasc, endereco);
 	}
-	
+
 	public String getNome() {
 		return this.nome;
 	}
 	public String getCpf() {
 		return this.cpf;
 	}
-	public String getClienteId() {
-		return this.ClienteId;
-	}
  	public void setNome(String nome) {
  		this.nome = nome;
  	}
  	public void setCpf(String cpf) {
  		this.cpf = cpf;
-	}
-	public void setClienteId(String ClienteId) {
-		this.ClienteId = ClienteId;
 	}
 }
 
