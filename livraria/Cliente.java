e57def13243512f61a28991d1b7c1e3a7d429c04
Phablulo <phablulo@live.com>
2017-11-26 17:26:27 -0300

    mais correções na classe de thomas

--- livraria/Cliente.java
+++ livraria/Cliente.java
@@ -1,9 +1,9 @@
 package livraria;
 
 public class Cliente extends Pessoa {
-	String nome;
-	String cpf;
-	String clienteId;
+	// String nome;
+	// String cpf;
+	// String clienteId;
 
 	/*Adequar os metodos e construtores a classe abstrata usando a keyword super.
 	 * Reforcando, o uso de Ids pode nao ser tao interessante, cpf seria uma alternativa melhor e mais rapida para implementar.*/
@@ -16,17 +16,17 @@ public class Cliente extends Pessoa {
 		super (nome, cpf, datanasc, endereco);
 	}
 
-	public String getNome() {
-		return this.nome;
-	}
-	public String getCpf() {
-		return this.cpf;
-	}
- 	public void setNome(String nome) {
- 		this.nome = nome;
- 	}
- 	public void setCpf(String cpf) {
- 		this.cpf = cpf;
-	}
+	// public String getNome() {
+	// 	return this.nome;
+	// }
+	// public String getCpf() {
+	// 	return this.cpf;
+	// }
+ 	// public void setNome(String nome) {
+ 	// 	this.nome = nome;
+ 	// }
+ 	// public void setCpf(String cpf) {
+ 	// 	this.cpf = cpf;
+	// }
 }
 
